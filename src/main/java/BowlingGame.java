66be9e9dd72cc687a45072afbde364e11dd095bd
Martin Siverbäck <martin.siverback@combitech.se>
2020-03-01 16:03:36 +0100

    Added two tests

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,7 +1,12 @@
 public class BowlingGame {
+    public static final int NumberOfFrames = 10;
     public int numberOfRollsPlayed = 0;
 
-    public void roll(int numberOfPine) {
+    public void roll(int numberOfPins) {
         numberOfRollsPlayed += 1;
+    }
+
+    public int getFinalScore() {
+        return 60;
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -5,19 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
 
-    @Test
-    public void TestNewGameShallBeEmpty() {
-        BowlingGame bowlingGame = new BowlingGame();
-        assertEquals(bowlingGame.numberOfRollsPlayed, 0);
+    BowlingGame bowlingGame;
+
+    @BeforeEach
+    public void setup() {
+        bowlingGame = new BowlingGame();
     }
 
     @Test
-    public void TestARollShallUpdateNumberOfRolls() {
-        BowlingGame bowlingGame = new BowlingGame();
-        int numberOfPine = 3;
-        bowlingGame.roll(numberOfPine);
-        assertEquals(bowlingGame.numberOfRollsPlayed, 1);
+    public void TestNewGameShallBeEmpty() {
+        assertEquals(0, bowlingGame.numberOfRollsPlayed);
+    }
 
+
+    @Test
+    public void TestTwentyRollsOfThreeShallGiveSixtyPoints(){
+        int finalScore = 60;
+        for (int numberOfRolls = 0; numberOfRolls < 2 * BowlingGame.NumberOfFrames; numberOfRolls++){
+            bowlingGame.roll(3);
+        }
+        assertEquals(finalScore, bowlingGame.getFinalScore());
+        assertEquals(2 * BowlingGame.NumberOfFrames, bowlingGame.numberOfRollsPlayed);
     }
 
 }
