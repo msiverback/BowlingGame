724494a257deb992a3c1323ada8e064c1fc82c6e
Martin Siverbäck <martin.siverback@combitech.se>
2020-03-01 16:17:49 +0100

    Can handle games without spares and strikes

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,12 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
     public static final int NumberOfFrames = 10;
     public int numberOfRollsPlayed = 0;
 
+    List<Integer> scores = new ArrayList();
+
     public void roll(int numberOfPins) {
         numberOfRollsPlayed += 1;
+        scores.add(numberOfPins);
+
     }
 
     public int getFinalScore() {
-        return 60;
+        return scores.stream().mapToInt(Integer::intValue).sum();
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,20 +15,43 @@ public class BowlingGameTest {
         bowlingGame = new BowlingGame();
     }
 
+
+    private void makeTwentyRolls(int numberOfPins) {
+        for (int numberOfRolls = 0; numberOfRolls < 2 * BowlingGame.NumberOfFrames; numberOfRolls++)
+            bowlingGame.roll(numberOfPins);
+    }
+
+
     @Test
     public void TestNewGameShallBeEmpty() {
         assertEquals(0, bowlingGame.numberOfRollsPlayed);
     }
 
-
+    @Test
+    public void TestTwentyRollsOfOneShallGiveTwentyPoints(){
+        int finalScore = 20;
+        int numberOfPins = 1;
+        makeTwentyRolls(numberOfPins);
+        assertEquals(finalScore, bowlingGame.getFinalScore());
+        assertEquals(2 * BowlingGame.NumberOfFrames, bowlingGame.numberOfRollsPlayed);
+    }
     @Test
     public void TestTwentyRollsOfThreeShallGiveSixtyPoints(){
         int finalScore = 60;
-        for (int numberOfRolls = 0; numberOfRolls < 2 * BowlingGame.NumberOfFrames; numberOfRolls++){
-            bowlingGame.roll(3);
-        }
+        int numberOfPins = 3;
+        makeTwentyRolls(numberOfPins);
         assertEquals(finalScore, bowlingGame.getFinalScore());
         assertEquals(2 * BowlingGame.NumberOfFrames, bowlingGame.numberOfRollsPlayed);
+    }
+
+    @Disabled("Disabled until two separeate scores can be handled")
+    public void TestTwentyOneRollsOfFiveShallGive150Points() {
+        int finalScore = 150;
+        int numberOfPins = 5;
+        makeTwentyRolls(numberOfPins);
+        bowlingGame.roll(5);
+        assertEquals(finalScore, bowlingGame.getFinalScore());
+        assertEquals(2 * BowlingGame.NumberOfFrames + 1, bowlingGame.numberOfRollsPlayed);
     }
 
 }
